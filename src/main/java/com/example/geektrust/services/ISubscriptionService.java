2c9ebc976a5bfed87fae21431771275220387d34
programmercave0 <mailabhi593@gmail.com>
2022-08-15 20:00:19 +0530

    create subscription service interface

---
+++ src/main/java/com/example/geektrust/services/ISubscriptionService.java
@@ -0,0 +1,18 @@
+package com.example.geektrust.services;
+
+import java.time.LocalDate;
+
+import com.example.geektrust.entity.Topup;
+import com.example.geektrust.entity.Plans.Plan;
+
+public interface ISubscriptionService {
+    public void createSubscription(LocalDate date);
+
+    public void addActivePlan(Plan plan);
+
+    public void printRenewDetails();
+
+    public void addTopUp(Topup topup);
+    
+    public Integer getPlanValue();
+}
